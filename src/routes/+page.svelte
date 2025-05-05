22cab51982cb062514b39aa6604e145a715c1b70
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-06 00:14:26 +0200

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { goto } from '$app/navigation';
+	import Button from '$lib/Button.svelte';
+	import Input from '$lib/Input.svelte';
+
+	let userName = $state('');
+	let password = $state('');
+
+	const btnclick = () => {
+		if (userName === 'admin' && password === 'admin') {
+			goto('/home');
+		} else {
+			alert('Login failed');
+		}
+	};
+</script>
+
+<div>
+	<Input bind:value={userName} placeholder="Brugernavn" />
+</div>
+<div>
+	<Input bind:value={password} placeholder="Kodeord" />
+</div>
+<div>
+	<Button onclick={btnclick}>Login</Button>
+</div>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,8 @@
+   
+    
+<h2 class="text-xl">Velkommen til din neurologiske dagbog </h2>
+<p>Her kan du registrere dine daglige symptomer og aktiviteter.</p>
+ <button class="btn btn-primary">Tryk her</button>
+
+
+    
