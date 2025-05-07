5afb504be086f9d1ccd8c0b0671d44fb40c426ca
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-07 22:54:35 +0200

    accent farve

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -2,4 +2,4 @@
 	let { children, onclick } = $props();
 </script>
 
-<button class="btn btn-primary m-2" {onclick}>{@render children()}</button>
+<button class="btn btn-accent m-2" {onclick}>{@render children()}</button>
