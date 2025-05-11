2723aa5d086dbe13e0818f7285a68b7689c69924
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 23:02:39 +0200

    Update +page.svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <h2 class="text-xl">Velkommen til din neurologiske dagbog </h2>
-<p>Her kan du registrere dine daglige symptomer og aktiviteter. På næste side finder du en liste af symptomer, vælg dem som passer til dig.</p>
+<p> Denne neurologiske dagbog er til dig med NF2. Her kan du registrere dine daglige symptomer og aktiviteter, som dig hos din læge/behandler kan følje med i. Du kan også bruge dagbogen til bedre at holde styr på dit sygdomsforløb mellem dine årlige syn.  På næste side finder du en liste af symptomer, vælg dem som passer til dig.</p>
 <button class="btn btn-accent" on:click={btnclick}>Tryk her</button>
 
 
