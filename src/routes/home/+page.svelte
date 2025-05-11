7259ccac374a39635996ea709d785539c223d84c
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 21:15:38 +0200

    Update +page.svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,10 +1,14 @@
-   
-    
+<script>
+  import { goto } from '$app/navigation';
+
+  const btnclick = () => {
+    goto('/symptomer');
+  };
+</script>
+
 <h2 class="text-xl">Velkommen til din neurologiske dagbog </h2>
 <p>Her kan du registrere dine daglige symptomer og aktiviteter.</p>
- <button class="btn btn-accent ">Tryk her</button>
-
-	const btnclick = () => {goto('/symptomer');
+<button class="btn btn-accent" on:click={btnclick}>Tryk her</button>
 
 
 
