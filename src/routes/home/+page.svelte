18ce2cac03ae2806695722197d9261b2f25bfe92
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 20:27:41 +0200

    Update +page.svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,7 +2,7 @@
     
 <h2 class="text-xl">Velkommen til din neurologiske dagbog </h2>
 <p>Her kan du registrere dine daglige symptomer og aktiviteter.</p>
- <button class="btn btn-primary">Tryk her</button>
+ <button class="btn btn-accent ">Tryk her</button>
 
 
     
