6c375e275a942525246a4a3b5096b79c3a138251
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 22:22:06 +0200

    Update +page.svelte

--- src/routes/symptomer/+page.svelte
+++ src/routes/symptomer/+page.svelte
@@ -7,6 +7,6 @@
 <h2 class="text-xl">Neurologisk symptom-dagbog for NF2</h2>
 <p> Nedenfor ses flere kategorier du kan have oplevet idag, klik på den du har oplevet. Har du oplevet mere, kan du kome tilbage efterfølgende og taste flere symptomer ind. </p>
 
-<button class="btn btn-accent" on:click={btnclick}>Forstoppelse</button>
+<button class="btn btn-accent" on:click={btnclick}>Høre vanskeligheder</button>
 <button class="btn btn-accent" on:click={btnclick}>Smerter</button>
 <button class="btn btn-accent" on:click={btnclick}>Epilepsi</button>
