fb135fab9bc0bd925fc46d17d3a1728a22f10b62
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 22:03:47 +0200

    Update +page.svelte

--- src/routes/symptomer/+page.svelte
+++ src/routes/symptomer/+page.svelte
@@ -4,8 +4,8 @@
   }
 </script>
 
-<h2 class="text-xl">Neurologisk symptom -dagbog for NF2</h2>
-<p> Nedenfor findes flere kategorier du kan have oplevet idag, klik på den du har oplevet. Har du oplevet mere, kan du kome tilbage efterfølgende og taste flere symptomer ind. </p>
+<h2 class="text-xl">Neurologisk symptom-dagbog for NF2</h2>
+<p> Nedenfor ses flere kategorier du kan have oplevet idag, klik på den du har oplevet. Har du oplevet mere, kan du kome tilbage efterfølgende og taste flere symptomer ind. </p>
 
 <button class="btn btn-accent" on:click={btnclick}>Forstoppelse</button>
 <button class="btn btn-accent" on:click={btnclick}>Smerter</button>
