4665fa827e99c80d738bb240863eb07a50ae5bb2
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 18:19:36 +0000

    Opdateret hjemmeside

--- Lektion3/src/lib/index.js
+++ Lektion3/src/lib/index.js
@@ -1,30 +1,30 @@
 <script>
   import { createSubscriber } from 'svelte/reactivity';
 
-  let username = sstate('user');
-  let password = sstate('password');
+  let username = $state('user');
+  let password = $state('password');
 
   const createUser = async () => {
-    const response = await fetch('/api/user',{
+    const response = await fetch('/api/user', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringfy({ ussername, password })
+      body: JSON.stringify({ username, password }) // Rettet stavefejl
     });
     await response.json();
     if (response.ok) {
-      alert('Fejl ved oprettelse af bruger!');
+      alert('Bruger oprettet succesfuldt!'); // Rettet besked
     } else {
       alert('Fejl ved oprettelse af bruger');
     }
     username = '';
     password = '';
   };
+</script>
+
 <div>
   <input type="text" bind:value={username} placeholder="Brugernavn" />
   <input type="password" bind:value={password} placeholder="Adgangskode" />
   <button class="btn btn-primary" onclick={createUser}>Opret bruger</button>
 </div>
-  
-</script>
