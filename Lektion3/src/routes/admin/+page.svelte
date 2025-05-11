e773abf509fa10104dd999f77f1926270f2827c1
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 16:18:17 +0200

    Update +page.svelte

--- Lektion3/src/routes/admin/+page.svelte
+++ Lektion3/src/routes/admin/+page.svelte
@@ -1,7 +1,18 @@
 <script>
-import { createSubscriber } from 'svelte/reactivity'; 
+  import { createSubscriber } from 'svelte/reactivity'; 
 
-let username = sstate('user');
-let password = sstate('password');
+  let username = $state('user');
+  let password = $state('password');
 </script>
+
+<h1 class="text-2xl font-bold">Administatorsiden</h1>
+<div>
+  <input type="text" bind:value={username} placeholder="Brugernavn" class="input" />
+</div>
+<div>
+  <input type="password" bind:value={password} placeholder="Adgangskode" class="input" />
+</div>
+<div>
+  <button class="btn btn-primary">Opret bruger</button>
+</div>
 
