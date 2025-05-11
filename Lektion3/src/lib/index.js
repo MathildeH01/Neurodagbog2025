a7b26935801fb36a69e8e3769d97f0022e206782
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 17:06:59 +0200

    Update index.js

--- Lektion3/src/lib/index.js
+++ Lektion3/src/lib/index.js
@@ -1,1 +1,24 @@
-// place files you want to import through the `$lib` alias in this folder.
+<script>
+  import { createSubscriber } from 'svelte/reactivity';
+
+  let username = sstate('user');
+  let password = sstate('password');
+
+  const createUser = async () => {
+    const response = await fetch('/api/user',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringfy({ ussername, password })
+    });
+    await response.json();
+    if (response.ok) {
+      alert('Fejl ved oprettelse af bruger!');
+    } else {
+      alert('Fejl ved oprettelse af bruger');
+    }
+    username = '';
+    password = '';
+  };
+</script>
