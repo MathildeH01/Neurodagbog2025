acdd0d9150edbfd3b418e050b84da2725a40b021
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 20:32:23 +0200

    Update api.js

--- Lektion3/src/lib/server/db/api/api.js
+++ Lektion3/src/lib/server/db/api/api.js
@@ -1,1 +1,30 @@
+<script>
+  import { createSubscriber } from 'svelte/reactivity';
 
+  let username = $state('user');
+  let password = $state('password');
+
+  const createUser = async () => {
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password }) // Rettet stavefejl
+    });
+    await response.json();
+    if (response.ok) {
+      alert('Bruger oprettet succesfuldt!'); // Rettet besked
+    } else {
+      alert('Fejl ved oprettelse af bruger');
+    }
+    username = '';
+    password = '';
+  };
+</script>
+
+<div>
+  <input type="text" bind:value={username} placeholder="Brugernavn" />
+  <input type="password" bind:value={password} placeholder="Adgangskode" />
+  <button class="btn btn-primary" onclick={createUser}>Opret bruger</button>
+</div>
