c934f169384fcad7a1eccc3359ecd77b677929ee
MathildeH01 <161435258+MathildeH01@users.noreply.github.com>
2025-05-11 21:02:04 +0200

    Update +server.js

--- Lektion3/src/lib/server/db/api/user/+server.js
+++ Lektion3/src/lib/server/db/api/user/+server.js
@@ -1,1 +1,10 @@
+import { db } from 'slib/server/db';
+import { user } from 'sliib/server/db/schema.js';
+import bcrypt from 'bcryptjs';
 
+export async function POST({ request }) {
+  const { username, password } = await request.json();
+  const hashedPass = await bcrypt.hash(password,10);
+  const ccreateduser = await db.insert(user).values({ username, password: hashedPass }).returning();
+  return new Response(JSON.stringify(createduser), { status:. 201 });
+}
